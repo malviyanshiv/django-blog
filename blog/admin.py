45dbefdb5cd61e9f3290984bb92a4255c5c0cea9
Shiv Shankar <malviyanshiv@gmail.com>
2019-01-13 08:27:35 +0530

    added model for comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Comment
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'publish', 'status', 'likes', 'comments')
+    list_display = ('title', 'slug', 'author', 'publish', 'status', 'like_count', 'comment_count')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug':('title',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('post', 'active', 'email')
+    odering = ('active', 'created')
+    search_fields = ('email', 'name', 'body')
--- blog/models.py
+++ blog/models.py
@@ -16,11 +16,32 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-    likes = models.IntegerField(default=0)
-    comments = models.IntegerField(default=0)
+    like_count = models.IntegerField(default=0)
+    comment_count = models.IntegerField(default=0)
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+#Published Post Manager for Post model
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+#Comment Model
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True) #to delete any comment by admin
+
+    class Meta:
+        ordering = ('created', )
+
+    def __str__(self):
+        return "Comment by {} on {}".format(self.name, self.post)
